405e3a71a743560148a7d1ac2d1456fbc989719a
Oleksii Petrashov <oleksii.petrashov@gmail.com>
2023-03-17 12:24:01 +0200

    Oleksii's arrange dir for review

---
+++ sort.py
@@ -0,0 +1,32 @@
+import sys
+from functions import get_file_list
+
+
+def do_cleaning():
+  scan_dir = None
+
+  if len(sys.argv) == 1:
+    print("Можна передати шлях до каталогу у якості праметра командного рядка наступним чином:")
+    print(f"\n\tpython {sys.argv[0]} <шлях_до_каталогу>\n")
+    print("або ввести з клавіатури нижче.\n")
+    try:
+      scan_dir = input("Введіть шлях до каталогу: ")
+    except KeyboardInterrupt:
+      print("")
+      exit()
+  else:
+    scan_dir = sys.argv[1]
+
+  try:
+    file_list = get_file_list(scan_dir)
+
+    for key, val in file_list.items():
+      print(f"{key} ->")
+      for each in val:
+        print(each)
+  except Exception as ex:
+    print(ex)
+
+
+if __name__ == "__main__":
+  do_cleaning()
